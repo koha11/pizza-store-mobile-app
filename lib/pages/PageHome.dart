8a06468bde8af98a52d5f8cb47e1a3ccf344177a
koha11 <khoa88108@gmail.com>
2025-05-02 20:27:38 +0700

    test pagehome and pagelogin

--- lib/pages/PageHome.dart
+++ lib/pages/PageHome.dart
@@ -20,28 +20,37 @@ class PageHome extends StatelessWidget {
           return SingleChildScrollView(
             child: Column(
               children: [
-                Stack(
+                Image.network("https://ilmio.vn/uploads/slider/10.png"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Image.network("https://ilmio.vn/uploads/slider/10.png"),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                Text("Your location"),
-                                Text("Nha Trang City"),
-                              ],
-                            ),
-                            Icon(Icons.search),
-                            Icon(Icons.notifications),
-                          ],
-                        ),
-                        Text("Provide the best"),
-                        Text("food for you"),
-                      ],
+                    Text(
+                      "Find by category",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
                     ),
                   ],
+                ),
+                GridView.count(
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                  childAspectRatio: 0.75,
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: List.generate(20, (index) {
+                    return Container(
+                      height: 200,
+                      color: Colors.grey[200],
+                      alignment: Alignment.center,
+                      child: Text("data $index"),
+                    );
+                  }),
                 ),
               ],
             ),
---
+++ lib/pages/PageLogin.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_store_app/pages/PageHome.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:get/get.dart';
+
+class PageLogin extends StatelessWidget {
+  PageLogin({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final emailTxt = TextEditingController();
+  final pwdTxt = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Login to your",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              "account.",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Please sign in to your account",
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: emailTxt,
+                      decoration: InputDecoration(labelText: "Email Address"),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: pwdTxt,
+                      decoration: InputDecoration(labelText: "Password"),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ButtonStyle(),
+                      onPressed: () async {
+                        final supabase = Supabase.instance.client;
+                        final AuthResponse res = await supabase.auth
+                            .signInWithPassword(
+                              email: emailTxt.text,
+                              password: pwdTxt.text,
+                            );
+
+                        final Session? session = res.session;
+                        final User? user = res.user;
+
+                        if (user != null) {
+                          Get.to(PageHome());
+                        }
+                      },
+                      child: Text("Sign In"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
