9967230f1ae4fe7ff154a88b93c0b48e32979234
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-09 15:49:28 +0700

    fix use user controller to get user's name & check login

--- lib/pages/PageProfile.dart
+++ lib/pages/PageProfile.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:pizza_store_app/controllers/controller_user.dart';
 import 'package:pizza_store_app/helpers/supabase.helper.dart';
+import 'package:pizza_store_app/pages/PageAddress.dart';
 import 'package:pizza_store_app/pages/PageChangeEmail.dart';
 import 'package:pizza_store_app/pages/PageChangeInfo.dart';
 import 'package:pizza_store_app/pages/PageChangePassword.dart';
@@ -17,118 +19,196 @@ class PageProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder(
-      init: HomePizzaStoreController.get(),
-      id: "1",
-      builder:
-          (controller) => Scaffold(
+      init: UserController(),
+      id: "user",
+      builder: (controller) {
+        if (controller.appUser == null && getCurrentUser() == null) {
+          return Scaffold(
             backgroundColor: Colors.white,
-            body: SingleChildScrollView(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
-                    children: [
-                      Icon(Icons.person),
-                      (getCurrentUser() == null
-                          ? GestureDetector(
-                            child: Text(
-                              "Bạn chưa đăng nhập, click để đăng nhập",
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.person),
+                  (getCurrentUser() == null
+                      ? GestureDetector(
+                        child: Text("Bạn chưa đăng nhập, click để đăng nhập"),
+                        onTap: () => Get.off(PageLogin()),
+                      )
+                      : Text(getCurrentUser()!.id)),
+                ],
+              ),
+            ),
+          );
+        }
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              "https://images.unsplash.com/photo-1633332755192-727a05c4013d?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
                             ),
-                            onTap: () => Get.off(PageLogin()),
-                          )
-                          : Text(getCurrentUser()!.id)),
-                      GestureDetector(
-                        child: Text("Đăng xuất"),
-                        onTap: () async {
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "${controller.appUser?.userName ?? ""}",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      getCurrentUser()?.email ?? "",
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                    SizedBox(height: 50),
+                    GestureDetector(
+                      onTap: () => Get.to(PageChangeInfo()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.person, size: 25),
+                              SizedBox(width: 15),
+                              Text(
+                                "Thông tin cá nhân",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios, size: 20),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    GestureDetector(
+                      onTap: () => Get.to(PageChangeEmail()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.email, size: 25),
+                              SizedBox(width: 15),
+                              Text(
+                                "Email",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios, size: 20),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    GestureDetector(
+                      onTap: () => Get.to(PageChangePassword()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.password, size: 25),
+                              SizedBox(width: 15),
+                              Text(
+                                "Mật khẩu",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios, size: 20),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    GestureDetector(
+                      onTap: () => Get.to(PageAddress()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.location_on, size: 25),
+                              SizedBox(width: 15),
+                              Text(
+                                "Địa chỉ",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios, size: 20),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
                           await controller.signOut();
+                          Get.off(PageLogin());
                         },
-                      ),
-                      Text(
-                        "Khánh Vinh",
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500,
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            side: BorderSide(color: Colors.grey, width: 1.5),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 12),
                         ),
-                      ),
-                      Text(
-                        getCurrentUser()!.email ?? "",
-                        style: TextStyle(color: Colors.grey, fontSize: 16),
-                      ),
-                      SizedBox(height: 50),
-                      GestureDetector(
-                        onTap: () => Get.to(PageChangeInfo()),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Row(
-                              children: [
-                                Icon(Icons.person, size: 25),
-                                SizedBox(width: 15),
-                                Text(
-                                  "Thông tin cá nhân",
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
+                            Icon(Icons.logout_outlined, size: 20),
+                            SizedBox(width: 10),
+                            Text(
+                              "Đăng xuất",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
-                            Icon(Icons.arrow_forward_ios, size: 20),
                           ],
                         ),
                       ),
-                      SizedBox(height: 30),
-                      GestureDetector(
-                        onTap: () => Get.to(PageChangeEmail()),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.email, size: 25),
-                                SizedBox(width: 15),
-                                Text(
-                                  "Thay đổi email",
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Icon(Icons.arrow_forward_ios, size: 20),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      GestureDetector(
-                        onTap: () => Get.to(PageChangePassword()),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.password, size: 25),
-                                SizedBox(width: 15),
-                                Text(
-                                  "Thay đổi mật khẩu",
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Icon(Icons.arrow_forward_ios, size: 20),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
+        );
+      },
     );
   }
 }
