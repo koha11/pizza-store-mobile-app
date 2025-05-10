e70fb7970df43971fbe9311baaed5cbd01d3331e
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2025-05-10 11:24:41 +0700

    update

---
+++ lib/pages/PageShopping_cart.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PageshoppingCart extends StatefulWidget {
+  const PageshoppingCart({super.key});
+
+  @override
+  State<PageshoppingCart> createState() => _PageshoppingCartState();
+}
+
+class _PageshoppingCartState extends State<PageshoppingCart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Giỏ hàng"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Muốn ăn lăn ra trang Home"),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+
+                },
+                child: Text("Tìm kiếm món ăn"))
+          ],
+        ),
+      ),
+    );
+  }
+}
