2e148135a79d3fa4722f4e7a344434e29e76833f
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-18 21:44:31 +0700

    fix display text, color & function to get order status name by string status

--- lib/enums/OrderStatus.dart
+++ lib/enums/OrderStatus.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 enum OrderStatus {
+  confirmed,
   pending,
   shipping,
   finished;
@@ -11,20 +12,34 @@ enum OrderStatus {
   // Thêm phương thức chuyển đổi từ String sang OrderStatus
   static OrderStatus fromString(String status) {
     return OrderStatus.values.firstWhere(
-          (e) => e.name == status,
+      (e) => e.name == status,
       orElse: () => OrderStatus.pending, // Giá trị mặc định nếu không khớp
     );
+  }
+
+  static String? fromDisplayTextToName(String status) {
+    if (status == "Tất cả") return null;
+    try {
+      final orderStatus = OrderStatus.values.firstWhere(
+        (e) => e.displayText == status,
+      );
+      return orderStatus.name;
+    } catch (e) {
+      return null;
+    }
   }
 
   // Thêm phương thức để hiển thị text tương ứng
   String get displayText {
     switch (this) {
+      case OrderStatus.confirmed:
+        return 'Đã xác nhận';
       case OrderStatus.pending:
-        return 'Chờ xử lý';
+        return 'Đang chờ';
       case OrderStatus.shipping:
         return 'Đang giao';
       case OrderStatus.finished:
-        return 'Hoàn thành';
+        return 'Đã hoàn thành';
       // case OrderStatus.cancelled:
       //   return 'Đã hủy';
     }
@@ -33,8 +48,10 @@ enum OrderStatus {
   // Thêm phương thức để lấy màu tương ứng
   Color get color {
     switch (this) {
+      case OrderStatus.confirmed:
+        return Colors.redAccent;
       case OrderStatus.pending:
-        return Colors.orange;
+        return Colors.purple;
       case OrderStatus.shipping:
         return Colors.blue;
       case OrderStatus.finished:
