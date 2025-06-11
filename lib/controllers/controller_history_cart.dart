cd3ca928041e1b59ffae23372d3f79df52e55ad0
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2025-06-11 10:39:07 +0700

    sort test order

--- lib/controllers/controller_history_cart.dart
+++ lib/controllers/controller_history_cart.dart
@@ -34,6 +34,7 @@ class HistoryCartController extends GetxController {
     // Lấy danh sách đơn hàng có status = 'pending'
     pendingOrders = await CustomerOrderSnapshot.getOrders(
       equalObject: {"customer_id": UserController.get().appUser!.userId},
+      sortObject: {"order_time": false},
     );
 
     isLoading = false;
