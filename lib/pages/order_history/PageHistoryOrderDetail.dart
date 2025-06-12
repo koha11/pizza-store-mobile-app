de0f6787abbe7698f7624eafb9b769feae7c2950
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2025-06-12 15:02:43 +0700

    fix remove history order

--- lib/pages/order_history/PageHistoryOrderDetail.dart
+++ lib/pages/order_history/PageHistoryOrderDetail.dart
@@ -185,47 +185,6 @@ class _PageHistoryOrderDetailState extends State<PageHistoryOrderDetail> {
                 ),
               ),
               const SizedBox(height: 24),
-              if (widget.order.status == OrderStatus.pending)
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        loadingDialog();
-                        await CustomerOrderSnapshot.deletePendingOrder(
-                          widget.order,
-                        );
-                        // Cập nhật lại danh sách đơn hàng
-                        await HistoryCartController.get().fetchPendingOrders();
-                        showSnackBar(desc: "Đã hủy đơn hàng!", success: true);
-                        Get.offAll(
-                          () => MainLayout(),
-                          binding: getRoleControllerBindings(""),
-                        ); // Quay lại trang danh sách đơn hàng
-                      } catch (e) {
-                        showSnackBar(
-                          desc: "Không thể hủy đơn hàng: $e",
-                          success: false,
-                        );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                    ),
-                    child: const Text(
-                      "Hủy đơn hàng",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
             ],
           ),
         ),
